1fb7a469609d9efd1cdb1a558919c8e7fcc3d271
Muhammad Nauman Azeem <43526417+Xeroxxhah@users.noreply.github.com>
2022-04-02 18:08:49 +0500

    feat:Show Locked Apps

--- appsafe.py
+++ appsafe.py
@@ -11,7 +11,7 @@ if dummy.IsFirst():
     print(f'Authentication Created:\nYour Default Password is: {DEFALUT_PASS}')
 
 
-VERSION='0.1.0'
+VERSION='0.2.1'
 option = ''
 
 def lock(path_to_binary):
@@ -25,7 +25,7 @@ def lock(path_to_binary):
         auth.validate()
         if auth.IsAuthenticated:
             app.lock(app.appname)
-            print(f'{app.appname} has been locked.')
+            print(f'{app.appname} has been locked successfully.')
         else:
             print('Authentication Failiure')
             quit()
@@ -34,10 +34,10 @@ def lock(path_to_binary):
 def unlock(path_to_binary):
     app = AppLock(path_to_binary)
     if not app.IsLocked():
-        print(f'{app.appname} is not locked successfully.')
+        print(f'{app.appname} is not locked.')
         quit()
     else:
-        password = getpass.getpass(prompt='Passworcd: ', stream=None)
+        password = getpass.getpass(prompt='Password: ', stream=None)
         auth = Auth(password)
         auth.validate()
         if auth.IsAuthenticated:
@@ -47,12 +47,17 @@ def unlock(path_to_binary):
             print('Authentication Failiure')
             quit()
 
+def ShowApps():
+    show = AppLock('core/agent')
+    show.showLockedApps()
+
 menu = """
 1: Lock
 2: Unlock
 3: Version
 4: Change Password
-5: Exit
+5: Show Locked Apps
+6: Exit
 """
 print(menu)
 option = input('Choose Option: ')
@@ -71,6 +76,8 @@ elif option == '4':
     chpass.validate()
     chpass.ChangePasswd(new_passwd)
 elif option == '5':
+    ShowApps()
+elif option == '6':
     print('Bye...')
     quit()
 else:
