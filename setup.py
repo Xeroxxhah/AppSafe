7e1d81a095f94105bd92739b6ad240087418df87
Muhammad Nauman Azeem <43526417+Xeroxxhah@users.noreply.github.com>
2022-05-22 23:22:54 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '1.4.0'
+VERSION = '1.4.1'
 DESCRIPTION = 'A Simple python AppLocker and FileLocker'
 LONG_DESCRIPTION = 'A Simple python AppLocker and FileLocker'
 
